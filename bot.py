4e0ad1b9e0cdc1955a44b4b18598b2b794feae2c
sbassoon <wrenstrom@gmail.com>
2017-09-11 20:02:45 +0200

    created OSC server with twitter-posting capabilities

---
+++ bot.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2015–2016 Molly White
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import tweepy
+import argparse
+import hashlib
+from secrets import *
+from time import gmtime, strftime
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+
+# ====== Individual bot configuration ==========================
+bot_username = 'sbotssoon'
+logfile_name = bot_username + ".log"
+
+# ==============================================================
+
+
+def tweet(text):
+    """Send out the text as a tweet."""
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    # Send the tweet and log success or failure
+    try:
+        api.update_status(text)
+    except tweepy.error.TweepError as e:
+        log(e.message)
+    else:
+        log("Tweeted: " + text)
+
+
+def log(message):
+    """Log message to logfile."""
+    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(path, logfile_name), 'a+') as f:
+        t = strftime("%d %b %Y %H:%M:%S", gmtime())
+        f.write("\n" + t + " " + message)
+
+
+def tweetOSC(data_type, args):
+    hash_me = str(data_type) + " " + str(args)
+    hashed = hashlib.sha3_512(hash_me.encode())
+    tweet(hashed.hexdigest())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip",
+        default="127.0.0.1", help="The ip to listen on")
+    parser.add_argument("--port",
+        type=int, default=5006, help="The port to listen on")
+    args = parser.parse_args()
+
+    dispatcher = dispatcher.Dispatcher()
+    dispatcher.map("/filter", tweetOSC)
+
+    server = osc_server.ThreadingOSCUDPServer(
+        (args.ip, args.port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
