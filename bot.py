e7f933ca5bcd569a5312ca5997cc912ca34ea577
sbassoon <wrenstrom@gmail.com>
2017-09-12 16:09:00 +0200

    completed hash parsing algorithm

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ import os
 import tweepy
 import argparse
 import hashlib
+import math
 import wordnik
 import re
 from secrets import *
@@ -29,10 +30,10 @@ from time import gmtime, strftime
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-
 # ====== Individual bot configuration ==========================
 bot_username = 'sbotssoon'
 logfile_name = bot_username + ".log"
+
 
 # ==============================================================
 
@@ -60,6 +61,7 @@ def log(message):
         t = strftime("%d %b %Y %H:%M:%S", gmtime())
         f.write("\n" + t + " " + message)
 
+
 def digital_root(n):
     x = sum(int(digit) for digit in str(n))
     if x < 10:
@@ -67,44 +69,75 @@ def digital_root(n):
     else:
         return digital_root(x)
 
-def sortHash(hash):
-    new_split_hash = ''
-    sorted_hash = ''.join(sorted(hash))
-    for entries in sorted_hash:
+
+def orderHash(hash):
+    ints_in_hash = ''
+    letters_in_hash = ''
+    slim_hash = ''
+    split_hash = (re.findall('\d+|\D+', hash))
+    for entries in split_hash:
         while True:
             try:
                 int(entries)
                 sum_hash_digits = digital_root(entries)
-                new_split_hash = new_split_hash + str(sum_hash_digits)
+                ints_in_hash = ints_in_hash + str(sum_hash_digits)
+                slim_hash = slim_hash + str(sum_hash_digits)
                 break
             except ValueError:
                 str(entries)
-                string_hash_letters = ''.join(sorted(set(entries)))
-                new_split_hash = new_split_hash + string_hash_letters
+                letters_in_hash = letters_in_hash + entries
+                slim_hash = slim_hash + entries
                 break
-    return new_split_hash
+
+    integers_in_hash = int(ints_in_hash)
+
+    digits = 1
+    hash_numerology = 0
+
+    if integers_in_hash > 0:
+        digits = int(math.log10(integers_in_hash)) + 1
+    elif integers_in_hash == 0:
+        digits = 1
+
+    if digits > 1:
+        hash_numerology = digital_root(ints_in_hash)
+    elif digits == 1:
+        hash_numerology = integers_in_hash
+
+    new_ordered_hash = str(hash_numerology) + letters_in_hash
+    return new_ordered_hash, ints_in_hash
+
+
+def sortAndOrderHash(hash):
+    sorted_hash = orderHash(hash)
+    ints_in_hash = sorted_hash[1]
+    resorted_hash_tuple = orderHash(''.join(set(sorted_hash[0])))
+    resorted_hash = resorted_hash_tuple[0]
+    return resorted_hash, ints_in_hash
 
 
 def tweetOSC(data_type, args):
     hash_me = str(data_type) + " " + str(args)
-    hashed = hashlib.md5(hash_me.encode())
-    hashedStr = hashed.hexdigest()
-    #tweet(hashedStr)
+    hashed_str = hashlib.md5(hash_me.encode()).hexdigest()
+    reduced_hash = sortAndOrderHash(hashed_str)
 
-    splitHash = (re.findall('\d+|\D+', hashedStr))
-
-    newHash = sortHash(splitHash)
-    print(newHash)
+    hash_int = int(reduced_hash[1])
+    hash_integer_array = [(hash_int//(10**i))%10 for i in range((int(math.ceil(math.log(hash_int, 10))) - 1), -1, -1)]
+    hash_list = (re.findall('\d+|\D+', reduced_hash[0]))
 
 
+
+
+
+    #tweet(reduced_hash[0])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip",
-        default="127.0.0.1", help="The ip to listen on")
+                        default="127.0.0.1", help="The ip to listen on")
     parser.add_argument("--port",
-        type=int, default=5006, help="The port to listen on")
+                        type=int, default=5006, help="The port to listen on")
     args = parser.parse_args()
 
     dispatcher = dispatcher.Dispatcher()
